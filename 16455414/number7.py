0215f442373c2e08598f04a74f960a0f373b99e4
mosma <mosmat1303@gmail.com>
2024-09-25 23:03:29 +0300

    change dictionary name

---
+++ 16455414/number7.py
@@ -0,0 +1,11 @@
+res = []
+for a in range(1000, 10000):
+    a = str(a)
+    c = []
+    c.append(str(int(a[0]) + int(a[1])))
+    c.append(str(int(a[1]) + int(a[2])))
+    c.append(str(int(a[2]) + int(a[3])))
+    d = c.sort(reverse=True)
+    if (c[1] + c[0]) == '1315':
+        print(a)
+        break
